fb86ad761fc69a8e65e937f0b75ec7a8db6e7920
vcis <san120786@gmail.com>
2022-07-13 21:20:33 +0530

    first commit

--- app/src/main/java/com/vcis/app/kotlinapplication/MainActivity.kt
+++ app/src/main/java/com/vcis/app/kotlinapplication/MainActivity.kt
@@ -55,4 +55,9 @@ class MainActivity : AppCompatActivity() {
         return navController.navigateUp(appBarConfiguration)
                 || super.onSupportNavigateUp()
     }
+
+    fun testcopy(){
+
+    }
+
 }
